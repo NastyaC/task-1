1f3c0ed4ab98e858e113ebc115e9f8b28e8caaa9
Anastasiia <aholopolos@gmail.com>
2016-08-08 13:06:48 +0300

    fixed errors

--- js/main.js
+++ js/main.js
@@ -1,74 +1,69 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var editInput =  document.createElement('input'),
-      inputValue = '',
-      sortedClass = 'sorted',
-      ths = Array.prototype.slice.call(document.getElementsByTagName('th')),
-      tableData = document.getElementsByTagName('tbody').item(0),
-      rowData = tableData.getElementsByTagName('tr'),
-      hasClass = function (elements, className) {
+  var editInput =  document.createElement('input');
+  var inputValue = '';
+  var sortedClass = 'sorted';
+  var table = document.getElementsByTagName('table').item(0);
+  var tableHeadElements = Array.prototype.slice.call(document.getElementsByTagName('th'));
+  var tableData = document.getElementsByTagName('tbody').item(0);
+  var rowData = tableData.getElementsByTagName('tr');
+  var hasClass = function (elements, className) {
         for (var i = 0; i < elements.length; i++) {
           if (elements[i].className === className) return true;
         }
 
         return false;
-      },
-      removeClass = function (elements) {
+      };
+  var removeClass = function (elements) {
         for (var i = 0; i < elements.length; i++) {
           elements[i].setAttribute('class', '');
         }
 
         return false;
-      },
-      removeRow = function (elements) {
-        var i = elements.length; 
-
-        while (i--) {
-
+      };
+  var removeRow = function (elements) {
+        for (var i = elements.length - 1; i >= 0; i--) {
           if(elements[i].getElementsByTagName('input')[0].checked) {
             elements[i].parentNode.removeChild(elements[i]);
           }
-
         }
 
         return false;
-      },
-      sortArray = function (a, b) {
-        var nameA = a[0].toLowerCase(),
-            nameB = b[0].toLowerCase();
+      };
+  var sortArray = function (element1, element2) {
+        var nameElement1 = element1[0].toLowerCase();
+        var nameElement2 = element2[0].toLowerCase();
 
-        if (nameA < nameB) return -1;
+        if (nameElement1 < nameElement2) return -1;
 
-        if (nameA > nameB) return 1;
+        if (nameElement1 > nameElement2) return 1;
 
         return 0; 
-      },
-      sortTable = function (element) {
-        var store = [],
-            i,
-            row,
-            sortTr;
+      };
+  var sortTable = function (element) {
+        var elementsStore = [];
+        var i;
+        var row;
+        var sortTr;
 
         for (i = 0; i < tableData.rows.length; i++) {
           row = tableData.rows[i];
-          sortTr = row.cells[ths.indexOf(element)].innerText;
-          store.push([sortTr, row]);
+          sortTr = row.cells[tableHeadElements.indexOf(element)].innerText;
+          elementsStore.push([sortTr, row]);
         }
 
-        store.sort(sortArray);
+        elementsStore.sort(sortArray);
 
-        if (hasClass(ths, sortedClass)) {
-          removeClass(ths);
-          store.reverse();
+        if (hasClass(tableHeadElements, sortedClass)) {
+          removeClass(tableHeadElements);
+          elementsStore.reverse();
         } else {
-          removeClass(ths);
+          removeClass(tableHeadElements);
           element.setAttribute('class', sortedClass);
         }
 
-        for (i = 0; i < store.length; i++) {
-          tableData.appendChild(store[i][1]);
+        for (i = 0; i < elementsStore.length; i++) {
+          tableData.appendChild(elementsStore[i][1]);
         }
-
-        store = null;
       };
 
   editInput.setAttribute('type', 'text');
@@ -79,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     td.innerHTML = this.value;
   };
 
-  document.querySelector('body').addEventListener('dblclick', function(event) {
+  table.addEventListener('dblclick', function(event) {
     var curElement = event.target;
 
     if (curElement.tagName.toLowerCase() === 'td') {
@@ -92,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   });
 
-  document.querySelector('body').addEventListener('click', function(event) {
+  table.addEventListener('click', function(event) {
     var curElement = event.target;
 
     if (curElement.tagName.toLowerCase() === 'th') {
@@ -100,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   });
 
-  document.getElementsByTagName('button')[0].addEventListener('click', function(){
+  document.getElementById('delete').addEventListener('click', function(){
     removeRow(rowData);
   });
 });
